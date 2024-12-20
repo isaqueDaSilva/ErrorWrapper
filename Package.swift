2c2e32fa00436630218fb536b76dd45bb38bd23a
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-12-20 05:13:53 -0300

    Changing minimum Platforms

--- Package.swift
+++ Package.swift
@@ -6,11 +6,11 @@ import PackageDescription
 let package = Package(
     name: "ErrorWrapper",
     platforms: [
-        .iOS(.v13),
+        .iOS(.v15),
         .macOS(.v12),
         .visionOS(.v1),
-        .watchOS(.v6),
-        .tvOS(.v13)
+        .watchOS(.v8),
+        .tvOS(.v15)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
